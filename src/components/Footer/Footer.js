c1fc5703f9790bedc61f93e23080f5181378fa3d
Juanma <juanmanuelandrada33@gmail.com>
2023-10-23 19:35:52 -0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,14 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='footer'>
+        <p className='footerelement'>Mail:juanmanuelandrada33@gmail.com</p>
+        <p className='footerelement'>Phone:+54 2994637844</p>
+        <a href='*' className='footerelement'>GitHub:https://github.com/eehjuanma</a>
+        <a href='*' className='footerelement'>LinkedIn:https://www.linkedin.com/in/juan-manuel-andrada-a07a1b21b/</a>
+      </div>
+    </div>
   )
 }
 
