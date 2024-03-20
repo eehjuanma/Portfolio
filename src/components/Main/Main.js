1419978e71e7c8347e4d9bda2ee38400508b6e50
Juanma <juanmanuelandrada33@gmail.com>
2024-03-20 13:48:04 -0300

    Gitignore

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,10 +12,16 @@ const Main = () => {
           <p>Jr. developer passionate about technology, being self-taught I have <br></br> learned programming 
             languages that allow me to develop my own projects <br></br> and fall in love with the world of free
            software.</p>
+           <div className='buttons'>
            <Link to='contact'><button className='button'>Let's Talk</button></Link>
            <Link to='projects'><button className='button2'>Projects</button></Link>
-           <Link><button className='button2'>GitHub</button></Link>
-           <Link to='projects'><button className='button2'>Linkedin</button></Link>
+           <div className='button buttondownload'>
+              <a className='textbutton' href='https://github.com/eehjuanma'>Git Hub</a>
+            </div>
+            <div className='button buttondownload'>
+              <a className='textbutton' href='https://www.linkedin.com/in/juan-manuel-andrada-a07a1b21b/'>LinkedIn</a>
+            </div>
+           </div>
         </div>
       </div>
     </div>
