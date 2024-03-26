c3406fa9393b9e73de76e9d5a3260de2ca0f2acc
Juanma <juanmanuelandrada33@gmail.com>
2024-03-25 22:08:21 -0300

    scroll snap

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import Ecommerce from './components/Ecommerce/Ecommerce';
+import { useRef } from "react";
+import useScrollSnap from "react-use-scroll-snap";
 
 function App() {
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 1000, delay: 20 });
   return (
     <div>
-      <div>
-      <Nav/>
-      <Main/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-      <div>
-        <Ecommerce/>
-      </div>
+      <div ref={scrollRef}>
+        <Nav/>
+        <Main/>
+        <About />
+        <Skills/>
+        <Projects/>
+        <Contact />
+        <Footer/>
       </div>
     </div>
   );
